0623e18f79bbe5abd7f9c0fd5cadaabb27e58084
Grace <gmilo92@haverfordsd.net>
2022-05-13 10:35:52 -0400

    restarted basically

--- attempt/GameScene.swift
+++ attempt/GameScene.swift
@@ -12,24 +12,49 @@ class GameScene: SKScene
 {
     
     
+    var button = SKSpriteNode(imageNamed: "playicon")
     
     override func didMove(to view: SKView)
     {
+        button.position = CGPoint(x: size.width * 0.1, y: size.height * 0.9)
+        button.name = "playButton"
+        button.size = CGSize(width: button.size.width * 0.8, height: button.size.height * 0.8)
+        button.isHidden = true
+        addChild(button)
         
-        // Get label node from scene and store it for use later
-        
-        
-        
-        // Create shape node to use during mouse interaction
-        
-    }
+        button = childNode(withName: "playButton") as! SKSpriteNode
+       
 
     }
     
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!.location(in: self)
+        
+        
+        
+        if button.frame.contains(touch)
+        {
+           
+        }
+    }
     
-func update(_ currentTime: TimeInterval)
+    
+    // Get label node from scene and store it for use later
+    func didMoveToView(view: SKView) {
+     
+
+    // Create shape node to use during mouse interaction
+
+}
+    
+    
+    
+    override func update(_ currentTime: TimeInterval)
 {
         // Called before each frame is rendered
 }
 
+    
+    
+}
---
+++ attempt/SecondScene.swift
@@ -0,0 +1,12 @@
+//
+//  SecondScene.swift
+//  attempt
+//
+//  Created by  on 5/13/22.
+//
+
+import UIKit
+
+class SecondScene: SKScene {
+
+}
