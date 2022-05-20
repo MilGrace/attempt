86272209412fcf4068c8ff9a5a7adafc33011c7d
Grace <gmilo92@haverfordsd.net>
2022-05-20 11:49:36 -0400

    more scenes

---
+++ attempt/HomeworkScene.swift
@@ -0,0 +1,32 @@
+//
+//  HomeworkScene.swift
+//  attempt
+//
+//  Created by  on 5/20/22.
+//
+
+import Foundation
+import SpriteKit
+
+class HomeworkScene: SKScene
+{
+    var returnH = SKSpriteNode(imageNamed: "returnH")
+    
+    override func didMove(to view: SKView)
+    {
+        returnH = childNode(withName: "returnH") as! SKSpriteNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let touch = touches.first!.location(in: self)
+        
+        if returnH.frame.contains(touch)
+        {
+            let returnScene = SecondScene(fileNamed: "SecondScene")
+            returnScene?.scaleMode = .aspectFill
+            let reveal = SKTransition.flipHorizontal(withDuration: 0)
+            view?.presentScene(returnScene!, transition: reveal)
+        }
+    }
+}
--- attempt/SecondScene.swift
+++ attempt/SecondScene.swift
@@ -11,11 +11,23 @@ import SpriteKit
 
 class SecondScene: SKScene
 {
+    var homework = SKSpriteNode(imageNamed: "homework")
     
     override func didMove(to view: SKView)
     {
-        
+        homework = childNode(withName: "homework") as! SKSpriteNode
     }
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let touch = touches.first!.location(in: self)
+        
+        if homework.frame.contains(touch)
+        {
+            let homeworkScene = HomeworkScene(fileNamed: "HomeworkScene")
+            homeworkScene?.scaleMode = .aspectFill
+            let reveal = SKTransition.flipHorizontal(withDuration: 0)
+            view?.presentScene(homeworkScene!, transition: reveal)
+        }
+    }
 }
